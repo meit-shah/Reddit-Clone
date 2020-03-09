d0cba68d255f05c5f18cc70820f277adfed62fce
Meit Shah <shah.j.meit@gmail.com>
2020-03-09 20:00:51 +0530

    Updates

---
+++ precache-manifest.021c9faf1aef0f36e065e180122214ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5690baad79bbfdf5d85fa553fa504a8a",
+    "url": "/Reddit-Clone/index.html"
+  },
+  {
+    "revision": "a5e5439461810abb6c81",
+    "url": "/Reddit-Clone/static/css/main.d6453f7e.chunk.css"
+  },
+  {
+    "revision": "9f579e8b0a3e00c3d2cc",
+    "url": "/Reddit-Clone/static/js/2.8e2c12f7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Reddit-Clone/static/js/2.8e2c12f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e5439461810abb6c81",
+    "url": "/Reddit-Clone/static/js/main.ebe5d87f.chunk.js"
+  },
+  {
+    "revision": "303ee046481ebcadd030",
+    "url": "/Reddit-Clone/static/js/runtime-main.4a37de65.js"
+  }
+]);
