cb7b3496e9e81580164d46362b8ef1ccbc63a60d
Meit Shah <shah.j.meit@gmail.com>
2020-03-09 18:25:35 +0530

    Initial Commit

---
+++ src/NewsCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class NewsCard extends React.Component {
+    render(){
+        return (
+            <div className="text-center">Hi, I am a News Card</div>
+        )
+    }
+}
+
+export default NewsCard;
